e23e4f1b6c4728791806a57569f5fc2ca0708639
王嘉庆 <12307130336@fudan.edu.cn>
2015-11-27 23:43:23 +0800

    添加底边栏

---
+++ app/src/main/java/com/danke7/hotel/EnterActivity.java
@@ -0,0 +1,59 @@
+package com.danke7.hotel;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RadioGroup;
+import android.widget.TabHost;
+
+public class EnterActivity extends TabActivity {
+    private TabHost tabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+
+        tabHost = this.getTabHost();
+
+        TabHost.TabSpec spec;
+        Intent intent;
+        intent=new Intent().setClass(this, SearchActivity.class);
+        spec=tabHost.newTabSpec("酒店").setIndicator("酒店").setContent(intent);
+        tabHost.addTab(spec);
+        intent=new Intent().setClass(this, OrderActivity.class);
+        spec=tabHost.newTabSpec("订单").setIndicator("订单").setContent(intent);
+        tabHost.addTab(spec);
+        intent=new Intent().setClass(this, ProfileActivity.class);
+        spec=tabHost.newTabSpec("我").setIndicator("我").setContent(intent);
+        tabHost.addTab(spec);
+
+        RadioGroup radioGroup=(RadioGroup) this.findViewById(R.id.main_tab_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.main_tab_search:
+                        tabHost.setCurrentTabByTag("酒店");
+                        break;
+                    case R.id.main_tab_order:
+                        tabHost.setCurrentTabByTag("订单");
+                        break;
+                    case R.id.main_tab_profile:
+                        tabHost.setCurrentTabByTag("我");
+                        break;
+                    default:
+                        tabHost.setCurrentTabByTag("酒店");
+                        break;
+                }
+            }
+        });
+    }
+}
